f44588ab4e518a09327c410fea4b8df1bf18e15d
DevSusu <os1742@gmail.com>
2021-01-02 22:27:51 +0900

    chore: add fastio to template

--- boj/main.cpp
+++ boj/main.cpp
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+#define fastio                        \
+   std::ios_base::sync_with_stdio(0); \
+   std::cin.tie(NULL);                \
+   std::cout.tie(NULL)
+
 int main() {
    return 0;
 }
